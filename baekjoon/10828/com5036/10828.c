a887269a4ec86b98c2af93e3044afe4e549fb71b
com5036 <qkrwl5036@naver.com>
2021-03-06 21:23:27 +0900

    10845

--- baekjoon/10828/com5036/10828.c
+++ baekjoon/10828/com5036/10828.c
@@ -2,27 +2,23 @@
 #include <string.h>
 #include <stdlib.h>
 
-int stack[10000];
-
-
-void push(char* str, int stack_cnt)
+void push(char* str, int stack_cnt, int *ptr)
 {
 	str = &str[1];
 	int num = atoi(str);
 
-	stack[stack_cnt] = num;
+	ptr[stack_cnt] = num;
 }
 
-int pop(int stack_cnt)
+int pop(int stack_cnt, int *ptr)
 {
 	if (stack_cnt == 0)
 	{
 		printf("-1\n");
 		return stack_cnt;
 	}
-	printf("%d\n", stack[stack_cnt - 1]);
+	printf("%d\n", ptr[stack_cnt - 1]);
 	return stack_cnt - 1;
-
 }
 
 void size(int stack_cnt)
@@ -38,7 +34,7 @@ void empty(int stack_cnt)
 		printf("0\n");
 }
 
-void top(int stack_cnt)
+void top(int stack_cnt, int *ptr)
 {
 	if (stack_cnt == 0)
 	{
@@ -46,8 +42,7 @@ void top(int stack_cnt)
 		return;
 	}
 
-	printf("%d\n", stack[(stack_cnt - 1)]);
-
+	printf("%d\n", ptr[(stack_cnt - 1)]);
 }
 
 void clearreadbuffer(void)
@@ -55,18 +50,15 @@ void clearreadbuffer(void)
 	while (getchar() != '\n');
 }
 
-
-
 int main(void)
 {
+	static int stack[10000];
 	int N;
 	int stack_cnt = 0;
-
 	char cmd[50];
 
 	scanf("%d", &N);
 	clearreadbuffer();
-
 
 	for (int i = 0; i < N; i++) {
 
@@ -74,33 +66,17 @@ int main(void)
 		cmd[strlen(cmd) - 1] = '\0';
 
 		if (strstr(cmd, "push") == cmd)
-		{
-			push(strstr(cmd, " "), stack_cnt++);
-
-		}
+			push(strstr(cmd, " "), stack_cnt++ ,stack);
 		else if (!strcmp(cmd, "pop"))
-		{
-			stack_cnt = pop(stack_cnt);
-		}
+			stack_cnt = pop(stack_cnt, stack);
 		else if (!strcmp(cmd, "size"))
-		{
 			size(stack_cnt);
-
-		}
 		else if (!strcmp(cmd, "empty"))
-		{
 			empty(stack_cnt);
-
-		}
 		else if (!strcmp(cmd, "top"))
-		{
-			top(stack_cnt);
-
-		}
+			top(stack_cnt, stack);
 		else
-		{
 			printf("ERROR\n");
-		}
 	}
 
 }
