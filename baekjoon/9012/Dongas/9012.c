1476eaa1546fbd74ce757d28e052ec655b33365f
BlueDongas <web3570@gmail.com>
2021-03-08 13:30:40 +0900

    9012.c 수정, README파일 추가

--- baekjoon/9012/Dongas/9012.c
+++ baekjoon/9012/Dongas/9012.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    int num = 0, i = 0, count = 0;  
-    scanf("%d",&num);
+int main() {
+    int num = 0, i = 0, j=0;
+    scanf("%d", &num);
 
-    for(i = 0;i<num;i++){
+    for (i = 0;i < num;i++) {
         char string[50];
-        scanf("%s",&string);
-        
+        scanf("%s", string);
+        int count = 0;
+
         int len = strlen(string);
-        
-        if(len >= 2){
-            if(string[i]=='('){
+
+        for (j = 0;j < len;j++) {
+            if (string[j] == '(') {
                 count++;
             }
-            else if(string[i]==')'){
-                if(count<=0){
-                    printf("NO\n");
-                    return 0;
+            else if (string[j] == ')') {
+                if (count <= 0) {
+                    count -= 100;
                 }
                 else count--;
             }
         }
-    }
-
-    if(count == 0){
-        printf("YES\n");
-    }else{
-        printf("NO\n");
+        if (count == 0) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
     }
 }
