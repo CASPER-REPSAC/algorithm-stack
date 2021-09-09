cb9507f378a44db108cd9ae967883d92599f49ab
Floodnut <gsniper777@gmail.com>
2021-09-09 20:37:34 +0900

    Create 9342.py

---
+++ baekjoon/9342/9342.py
@@ -0,0 +1,16 @@
+import re, sys
+
+regex = re.compile('[A-F]{0,1}A+F+C+[A-F]{0,1}$')
+wordCounts = int(input())
+words = []
+
+for repeat in range(wordCounts):
+    a = sys.stdin.readline().strip()
+    words.append(a)
+
+for isInfected in words:
+    isMatch = regex.match(isInfected)
+    if(isMatch != None):
+        print('Infected!')
+    else:
+        print('Good')
