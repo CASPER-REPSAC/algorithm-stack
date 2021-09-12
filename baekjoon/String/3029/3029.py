a13d1f51b077ad9a2d34e6c3ec9754931ca588e6
pon04178 <pon04178@naver.com>
2021-09-12 11:17:18 +0900

    3029 solved

--- baekjoon/String/3029/3029.py
+++ baekjoon/String/3029/3029.py
@@ -1,7 +1,18 @@
 import sys
-sys.stdin = open("input.txt","rt")
+sys.stdin = open("algorithm-stack\\baekjoon\String\\3029\input.txt","rt")
 
-s = list(int(input().split(':')))
-e = list(int(input().split(':')))
+s = list(map(int, input().split(":")))
+e = list(map(int, input().split(":")))
 
-print(s, e)
+st = s[0]*60*60 + s[1]*60 + s[2]
+et = e[0]*60*60 + e[1]*60 + e[2]
+
+if et > st:
+    t = et - st
+else:
+    t = et - st + (24*60*60)
+
+h = t // 60 // 60
+m = t // 60 % 60
+s = t % 60
+print("%02d:%02d:%02d" % (h,m,s))
