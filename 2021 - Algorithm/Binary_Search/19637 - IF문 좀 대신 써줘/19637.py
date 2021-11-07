2876c40ba69ee5b64fc216eb0ccda564d457b638
BlueDongas <web3570@gmail.com>
2021-11-07 21:35:55 +0900

    plus 주석

--- 2021 - Algorithm/Binary_Search/19637 - IF문 좀 대신 써줘/19637.py
+++ 2021 - Algorithm/Binary_Search/19637 - IF문 좀 대신 써줘/19637.py
@@ -6,7 +6,7 @@ def checkname(name,data):
     result = 0
     start,end = 0, len(name)-1
 
-    while start<=end:
+    while start<=end:  #Binary Search 알고리즘
         mid = (start+end)//2
         if int(name[mid][1]) >= data :
             end = mid-1
@@ -17,6 +17,7 @@ def checkname(name,data):
 
 n,m = map(int,input().split())
 name = [input().split() for _ in range(n)]
+
 for _ in range(m):
     data = int(input())
     print(checkname(name,data))
--- 2021 - Algorithm/Tree_Traversal/2606-바이러스/2606.py
+++ 2021 - Algorithm/Tree_Traversal/2606-바이러스/2606.py
@@ -11,15 +11,15 @@ for _ in range(m):
     a,b = map(int,input().split())
     a -=1
     b -=1
-    relation[a][b] = relation[b][a] = 1
+    relation[a][b] = relation[b][a] = 1 #연결되었다는 것을 1로 표현
 
 count = []
 def dfs(i):
-    infection[i]=1
+    infection[i]=1 #감염 되었다는 것을 표현
     for j in range(n):
-        if relation[i][j] == 1 and infection[j] == 0:
+        if relation[i][j] == 1 and infection[j] == 0: #감염컴퓨터와 연결되었지만 감염x 일때
             count.append(j)
-            dfs(j)
+            dfs(j) 
     return len(count)
 
 print(dfs(0))
